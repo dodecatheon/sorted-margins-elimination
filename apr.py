9bcd89ec6c388734152bd09fb9e6aff6b292ba00
Ted Stern <lostern@microsoft.com>
2019-07-11 09:30:04 -0700

    some experiments

---
+++ apr.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python
+# % matplotlib notebook
+# import matplotlib
+# % config InlineBackend.figure_format = 'svg'
+# % matplotlib inline
+# import matplotlib.pyplot as plt
+import numpy as np
+from math import *
+import csv
+from pprint import pprint
+from csvtoballots import csvtoballots
+
+__doc__ = """\
+smeminlv --
+Sorted Margins Elimination, Minimum Losing Votes (equal-rated whole) --
+Returns ordered ranking with Condorcet (beats-all) winner as 0-th entry if one exists.
+"""
+
+def pairwise_erw(ballots, weight):
+    # Important: Tied votes above the minimum ballot score are counted
+    # as whole votes for and against (Equal-Rated Whole = ERW). Note
+    # that the pairwise array's resulting diagonal is effectively the
+    # total approval score for each candidate
+
+    numballots, numcands = np.shape(ballots)
+    maxscore = ballots.max()
+    totalweight = weight.sum()
+    A = np.zeros((numcands,numcands))
+    maxscorep1 = maxscore + 1
+    for ballot, w in zip(ballots,weight):
+        vmin = int(ballot.min() + 1.5)
+        for v in range(vmin,maxscorep1):
+            A += np.multiply.outer(np.where(ballot==v,w,0),
+                                   np.where(ballot<=v,1,0))
+    return(totalweight,numcands,A)
+
+def mycompress(arr):
+    return(np.compress(arr>=0,arr))
+
+def sme_minlv(ballots, weight, cands,cnames=[],scalar=False):
+    "Sorted Margins Elimination, MinLV (erw)"
+    # erw = equal rated whole:
+    # tied approval votes are counted as equal votes for each candidate
+    # Enter a cnames list of the same length as cands to turn on verbosity
+
+    # Returns candidates, sorted by method
+    ncands = len(cands)
+    verbose = (len(cnames) > 0)
+
+    # Terminate recursion at 1 or 2 candidates:
+    if ncands == 1:
+        if scalar:
+            return(cands[0])
+        else:
+            return(cands)
+
+    totalweight, numcands, AA = pairwise_erw(ballots[:,cands],weight)
+
+    if ncands == 2:
+        # Ties preserve input order
+        if AA[1,0] > AA[0,1]:
+            if scalar:
+                return(cands[-1])
+            else:
+                return(cands[::-1])
+        else:
+            if verbose and (AA[0,1] == AA[1,0]):
+                print("Candidates {} and {} are tied".format(cnames[cands[0]],
+                                                             cnames[cands[1]]))
+            if scalar:
+                return(cands[0])
+            else:
+                return(cands)
+
+    # track the losing votes locations
+    losing_votes = np.where(AA.T > AA, AA, -1)
+    np.fill_diagonal(losing_votes,-1)
+
+    tied_votes = np.where(AA.T == AA, AA, -1)
+    np.fill_diagonal(tied_votes,-1)
+
+    wintie_votes = np.where(AA.T <= AA, AA, -1)
+    np.fill_diagonal(wintie_votes,-1)
+
+    if verbose:
+        print("----------")
+        print("Permuted pairwise scores:\n    ", [cnames[q] for q in cands])
+        for i, rowAA in enumerate(AA):
+            print(cnames[cands[i]], rowAA)
+        print("----------")
+
+    # Set up for a descending sort by losing votes:
+    scorelist = []
+    for (c,
+         rowLV, # rowWV,
+         rowTV,
+         rowWT,
+         rowAA) in zip(cands,
+                       losing_votes,
+                       tied_votes,
+                       wintie_votes,
+                       AA):
+
+        cc = np.array([c])
+        cands_minus_c = np.compress(cands!=c,cands)
+
+        WTcompress = mycompress(rowWT)
+
+        if len(WTcompress) == 0:
+            # shortcut return if we find a defeated-by-all loser:
+            if verbose:
+                print("Candidate {} is defeated by all other candidates".format(cnames[c]),
+                      [cnames[q] for q in cands_minus_c])
+            if scalar:
+                return(sme_minlv(ballots, weight, cands_minus_c, cnames, scalar))
+            else:
+                return(np.concatenate((sme_minlv(ballots, weight, cands_minus_c, cnames), cc)))
+
+        LVcompress = mycompress(rowLV)
+
+        if len(LVcompress) > 0:
+            LVmin = LVcompress.min()
+            scorelist.append(LVmin)
+            AAmin = rowAA.min()
+            if (verbose and ( LVmin != AAmin )):
+                print(("*** Candidate {} has different "
+                       "Minimum Pairwise Support ({}) than MinLV ({})").format(cnames[c],
+                                                                               AAmin,
+                                                                               LVmin))
+        else:
+            if len(mycompress(rowTV)) == 0:
+                # Terminate recursion if we find a beats-all winner
+                if verbose:
+                    print("Candidate {} defeats all candidates".format(cnames[c]),
+                          [cnames[q] for q in cands_minus_c])
+                # Shortcut return
+                if scalar:
+                    return(c)
+                else:
+                    return(np.concatenate((cc, sme_minlv(ballots, weight, cands_minus_c, cnames))))
+            else:
+                if verbose:
+                    print("Candidate {} is tied with candidates".format(cnames[c]),
+                          [cnames[q] for q in np.compress(rowTV>=0,cands)])
+                scorelist.append(WTcompress.min())
+                # return(False)
+
+    # convert scorelist to np.array:
+    minlv = np.array(scorelist)
+
+    # position in cands when sorted by minimum losing score in descending order
+    arglvsort = minlv.argsort()[::-1]
+
+    if verbose:
+        print("Sorted MinLV candidates:")
+        pprint([z for z in zip([cnames[cands[q]] for q in arglvsort],
+                               minlv[arglvsort])])
+
+    # Loop over the minlv array, looking for out-of-(pairwise-defeat-)order pairs
+    while True:
+        lvsort = minlv[arglvsort]
+        lvdiff = []
+        outoforder = []
+        mindiffval = lvsort[0] - lvsort[ncands-1]
+        mindiff = ncands
+        # Loop backwards through the sorted array.
+        # The mindiff calculation then ensures that tied differences
+        # are resolved with preference to lower ranked pairs
+        for i in range(ncands-1,0,-1):
+            im1 = i - 1
+            c_i = arglvsort[i]
+            c_im1 = arglvsort[i-1]
+            if AA[c_i,c_im1] > AA[c_im1,c_i]:
+                outoforder.append(im1)
+                lvdiff.append(lvsort[im1] - lvsort[i])
+                if lvdiff[-1] < mindiffval:
+                    mindiff = im1
+                    mindiffval = lvdiff[-1]
+
+        # terminate loop when no more pairs are out of order pairwise.
+        if (len(outoforder) == 0) or (mindiff == ncands):
+            break
+
+        # find the minimum pairwise out of order pair, sorted by minimum losing votes
+        # mindiff = outoforder[sorted(range(len(outoforder)),key=lvdiff.__getitem__)[0]]
+
+        if verbose:
+            print(("Swapping candidates {} and {} "
+                   "at minimum pairwise out-of-order "
+                   "MinLV difference").format(cnames[cands[arglvsort[mindiff]]],
+                                              cnames[cands[arglvsort[mindiff+1]]]))
+
+        # ... and swap their order
+        arglvsort[range(mindiff,mindiff+2)] = arglvsort[range(mindiff+1,mindiff-1,-1)]
+        if verbose:
+            print("New candidate order:", [cnames[q] for q in cands[arglvsort]])
+
+    # We can terminate recursion here if ncands == 3
+    if ncands == 3:
+        winner, runnerup = arglvsort[:2]
+        if verbose and (AA[winner,runnerup] == AA[runnerup,winner]):
+            print("Candidates {} and {} are tied".format(cnames[cands[winner]],
+                                                         cnames[cands[runnerup]]))
+        if scalar:
+            return(cands[arglvsort[0]])
+        else:
+            return(cands[arglvsort])
+
+    # Once the array is sorted, eliminate the last candidate and run again
+    # on the remaining candidates recursively
+    if verbose:
+        print("Eliminating lowest-ordered candidate {}".format(cnames[cands[arglvsort[-1]]]))
+        print("Running sme_minlv on candidates", [cnames[q] for q in cands[arglvsort[:-1]]])
+
+    if scalar:
+        return(sme_minlv(ballots,weight,cands[arglvsort[:-1]],cnames,scalar))
+    else:
+        return(np.concatenate((sme_minlv(ballots,
+                                         weight,
+                                         cands[arglvsort[:-1]],
+                                         cnames),
+                               cands[[arglvsort[-1]]])))
+
+def test_sme(ballots,weight,cnames,scalar=False):
+    numcands = np.shape(ballots)[1]
+    cands = np.arange(numcands)
+    if scalar:
+        print("SME_MinLV winner =", cnames[sme_minlv(ballots,
+                                                     weight,
+                                                     cands,
+                                                     cnames,
+                                                     scalar=scalar)])
+    else:
+        winsort = sme_minlv(ballots,weight,cands,cnames)
+        print("SME_MinLV ranking = ",
+              " > ".join([cnames[winsort[k]] for k in range(numcands)]))
+
+        # Test whether LIIA is violated (i.e., if SME_MinLV with winner excluded
+        # is the same as the runner up from the full contest)
+
+        if numcands > 2:
+            winner, runnerup = winsort[0:2]
+
+            cands_minus_winner = np.compress(cands!=winner,cands)
+
+            newsort = sme_minlv(ballots,weight,cands_minus_winner)
+            newwinner = newsort[0]
+
+            if (newwinner != runnerup):
+                print(("*** Potential LIIA violation found ***\n"
+                       "Winner with winner {} excluded: {}; "
+                       "original runner up: {}").format(cnames[winner],
+                                                        cnames[newwinner],
+                                                        cnames[runnerup]))
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument("-i", "--inputfile",
+                        type=str,
+                        required=True,
+                        help="REQUIRED: CSV Input file [default: none]")
+    parser.add_argument("-w", "--winneronly",
+                        action='store_true',
+                        default=False,
+                        help="Calculate winner-only, no ranking [default: False]")
+
+    args = parser.parse_args()
+
+    ballots, weight, cnames = csvtoballots(args.inputfile)
+
+    # Figure out the width of the weight field, use it to create the format
+    ff = '{{:{}d}}:'.format(int(log10(weight.max())) + 1)
+
+    print("{}, {}".format('weight',','.join(cnames)))
+    for ballot, w in zip(ballots,weight):
+        print(ff.format(w),ballot)
+
+    test_sme(ballots,weight,cnames,scalar=args.winneronly)
+
+if __name__ == "__main__":
+    main()
--- ir3f2.py
+++ ir3f2.py
@@ -29,21 +29,21 @@ def smith_from_losses(losses,cands):
 # Check for qualifying, then if any pass, add them to the ranking
 # As qualifying candidates are found, add their (r,s,t) ratings
 # to the ratings dict
-def checkQ(r,s,TG,MXvec,remaining,ratings,ranking):
+def checkQ(r,TG,MXvec,remaining,ratings,ranking):
     Q = []
     for c in remaining:
         t = TG[c]
         mx = MXvec[c]
         if (mx == 0) or (t > mx):
             Q.append(c)
-            ratings[c] = (r,s,t)
+            ratings[c] = (r,t)
 
     if len(Q) > 0:
         for q in Q:
             remaining.discard(q)
         ranking += sorted(Q,key=(lambda c:TG[c]),reverse=True)
 
-def smithrr(ballots, weight):
+def ir3f2(ballots, weight):
     """Relevant Rating is an analog of Majority Judgment based on Chris
     Benham's IBIFA method, but does not pass LaterNoHelp because rating
     thresholds depend on scores given to other candidates.
@@ -66,72 +66,31 @@ def smithrr(ballots, weight):
     maxscore = ballots.max()
     maxscorep1 = maxscore + 1
 
-    # A: pairwise array, equal-rated-none
-    # TEQ:  where X is equal-rated top with Y
-    # BEQ:  where X is equal-rated bottom with Y
-    # X:  for score r = 0..maxscore, save approval for X when score for Y is below r
-    # T:  Total approval at score r = 0..maxscore for candidate X, except T[0] is zero
-    A = np.zeros((ncands,ncands))
-    TEQ = np.zeros((ncands,ncands))
-    BEQ = np.zeros((ncands,ncands))
-    X = np.zeros((maxscorep1,ncands,ncands))
-    T = np.zeros((maxscorep1,ncands))
-
     # ----------------------------------------------------------------------
     # Tabulation:
     # ----------------------------------------------------------------------
+    # X:  for score r = 0..maxscore, save approval for X when score for Y is below r
+    # T:  Total approval at score r = 0..maxscore for candidate X, except T[0] is zero
+    B = np.zeros((maxscorep1,ncands,ncands))
+    X = np.zeros((maxscorep1,ncands,ncands))
+    T = np.zeros((maxscorep1,ncands))
     for ballot, w in zip(ballots,weight):
         rmin = int(ballot.min() + 0.5)
         rmax = int(ballot.max() + 0.5)
- 
-        TEQ += np.multiply.outer(np.where(ballot==rmax,w,0),
-                                 np.where(ballot==rmax,1,0))
-
-        BEQ += np.multiply.outer(np.where(ballot==rmin,w,0),
-                                 np.where(ballot==rmin,1,0))
-
         for r in range(rmax,rmin,-1):
-            A += np.multiply.outer(np.where(ballot==r,w,0),
-                                   np.where(ballot<r ,1,0))
-
-            X[r] += w * np.multiply.outer(np.where(ballot<r,1,0),
-                                          np.where(ballot>0,1,0))
-
-            T[r] += w * np.where(ballot==r,1,0)
+            B[r] += np.multiply.outer(np.where(ballot==r,w,0),
+                                      np.where(ballot<r ,1,0))
+            X[r] += np.multiply.outer(np.where(ballot<r,w,0),
+                                      np.where(ballot>0,1,0))
+            T[r] += np.where(ballot==r,w,0)
 
     for r in range(1,maxscorep1):
         np.fill_diagonal(X[r],0)
 
-    np.fill_diagonal(TEQ,0)
-    np.fill_diagonal(BEQ,0)
-    # ----------------------------------------------------------------------
-    # Rank and rating calculations:
-    # ----------------------------------------------------------------------
-    # Condorcet and Improved Condorcet calculations:
-    # Find the Smith set (the set of candidates that defeats all candidates
-    # outside the set), the set of candidates that are undefeated when
-    # Equal-Top scores are used, and the set of candidates that are undefeated
-    # when both equal-top and equal-bottom scores are used symmetrically.
 
-    # Calculate Smith, TEQ undefeated, symmetric undefeated:
-    smith = smith_from_losses(np.where(A.T > A, 1, 0),cands)
-    
-    lossTEQ = np.where(A.T > (A + TEQ), 1, 0)
-    losssym = np.where((A.T + BEQ) > (A + TEQ), 1, 0)
-
-    TEQsum = lossTEQ.sum(axis=1)
-    minTEQ = lossTEQ.min()
-    symsum = losssym.sum(axis=1)
-    minsym = symsum.min()
-
-    undefeated_TEQ = set(np.compress(TEQsum==minTEQ,cands))
-    undefeated_sym = set(np.compress(symsum==minsym,cands))
-
-    if (len(undefeated_TEQ) == 0):
-        undefeated_TEQ = set(cands)
-
-    if (len(undefeated_sym) == 0):
-        undefeated_sym = set(cands)
+    # A: pairwise array, equal-rated-none
+    # B: pairwise array, equal-rated-none, for candidates with rate r
+    A = B.sum(axis=0)
 
     # ----------------------------------------------------------------------
     # Relevant Ratings:
@@ -139,7 +98,7 @@ def smithrr(ballots, weight):
     # MX[r,i] is the maximum approval for any candidate on ballots that
     # rate candidate below rating r
     MX = X.max(axis=2)
-
+ 
     # ... and MXC is the candidate with that maximum excluded approval
     MXC = X.argmax(axis=2)
 
@@ -155,71 +114,94 @@ def smithrr(ballots, weight):
 
     # Ratings significance:  
     # 
-    # ratings[c] = (r,s,t)
+    # ratings[c] = (r,t)
     #   
-    # In each triple (r,s,t) for candidate c:
+    # In each pair (r,t) for candidate c:
     # 
     # When r is greater than 0,
-    # r is the rating level at which 
-    # "t", the total number of ballots rating candidate c at level s and above
-    # exceeds the maximum approval for any candidate rating c below r.
+    # r is the rating level at which "t", the total number of ballots
+    # rating candidate c at level r and above, exceeds the maximum
+    # approval for any candidate rating c below r.
 
     # If r equals zero, then candidate c was not able to pass any qualifying threshold 
     # and was sorted at the end of the ranking by total approval.
 
     while (len(remaining) > 0):
         TG += T[r]            # Note that the T[0] list is all zeros, by construction above.
-        s = r                 # "s" is used to save the ratings level used in TG
-        checkQ(r,s,TG,MX[r],remaining,ratings,ranking)
+        checkQ(r,TG,MX[r],remaining,ratings,ranking)
 
         if len(remaining) > 0:
             r -= 1
-            # checkQ(r,s,TG,MX[r],remaining,ratings,ranking)
-            # Note that this terminates when r == 0, since MX[0] is a list of zeros
-            # by construction
 
-    # For Benham's IBIFA method, just comment out the second checkQ in the while loop
     # ----------------------------------------------------------------------
-    # Use Relevant Ratings to sort the Condorcet sets:
+    # Use IBIFA (Relevant Ratings) to sort the Condorcet sets:
     # ----------------------------------------------------------------------
     # invert the rankings list:  rrindex[i] is the placement in the rankings for candidate i
     rrindex = [r for r in ranking]
     for i, r in enumerate(ranking):
         rrindex[r] = i
 
-    rr_smith = sorted(list(smith),key=(lambda c:rrindex[c]))
-    rr_TEQ = sorted(list(undefeated_TEQ),key=(lambda c:rrindex[c]))
-    rr_sym = sorted(list(undefeated_sym),key=(lambda c:rrindex[c]))
+    appr_level = ratings[ranking[0]][0]
 
-    return(tw,ncands,maxscore,ratings,ranking,rr_smith,rr_TEQ,rr_sym,A,MX,MXC)
+    A2 = B[appr_level:].sum(axis=0)
 
-def test_smithrr(ballots,weight,cnames):
+    Approval = T[appr_level:].sum(axis=0)
+
+    # ----------------------------------------------------------------------
+    # Rank and rating calculations:
+    # ----------------------------------------------------------------------
+    # Find the Smith set (the set of candidates that defeats all candidates
+    # outside the set)
+    fullsmith = smith_from_losses(np.where(A.T > A, 1, 0),cands)
+    # Find the Smith set using only inferred Approval ratings
+    apprsmith = smith_from_losses(np.where(A2.T > A2, 1, 0), cands)
+
+    if (len(fullsmith) == 1):
+        winner = list(fullsmith)[0]
+    elif (len(apprsmith) == 1):
+        winner = list(apprsmith)[0]
+    else:
+        winner = sorted(cands,key=(lambda c:Approval[c]),reverse=True)[0]
     
-    tw,ncands,maxscore,ratings,ranking,rr_smith,rr_TEQ,rr_sym,A,MX,MXC = smithrr(ballots,weight)
+    return(winner,tw,ncands,maxscore,ratings,ranking,list(fullsmith),list(apprsmith),Approval,A,A2,MX,MXC)
+ 
+def test_ir3f2(ballots,weight,cnames):
+    
+    winner,tw,ncands,maxscore,ratings,ranking,fullsmith,apprsmith,approval,A,A2,MX,MXC = ir3f2(ballots,weight)
 
-    print("\nPairwise Array:")
+    cands = np.arange(ncands)
+
+    print("\nFull Pairwise Array:")
     for row in A:
         print(row)
 
-    print("\nRelevant Ratings:\n")
+    print("\nApproved Pairwise Array:")
+    for row in A2:
+        print(row)
+
+    print("\nApproval:")
+    print(approval)
+
+    print("\nIBIFA Ratings:")
     for c in ranking:
-        r, s, t = ratings[c]
+        r, t = ratings[c]
         mx = MX[r,c]
         mxc = MXC[r,c]
-        print("\t{}: ({}, {}, {}), {}: {}".format(cnames[c],r,s,t,cnames[mxc],mx))
+        print("\t{}: ({}, {}), {}: {}".format(cnames[c],r,t,cnames[mxc],mx))
 
-    print("\nRelevant Ratings ranking:\n")
-    print("\t{}".format(' > '.join([cnames[c] for c in ranking])))
+    print("\nIBIFA ranking:")
+    print("\t{}".format('>'.join([cnames[c] for c in ranking])))
         
-    print("\nSmith//RR:\n")
-    print("\t{}".format(' > '.join([cnames[c] for c in rr_smith])))
+    print("\nFull Smith:\n")
+    print("\t{}".format(','.join([cnames[c] for c in fullsmith])))
 
-    print("\nTEQ//RR:\n")
-    print("\t{}".format(' > '.join([cnames[c] for c in rr_TEQ])))
+    print("\nApproval Smith:\n")
+    print("\t{}".format(','.join([cnames[c] for c in apprsmith])))
 
-    print("\nSym//RR:\n")
-    print("\t{}".format(' > '.join([cnames[c] for c in rr_sym])))
+    print("\nApproval rankings:\n")
+    print("\t{}".format('>'.join([cnames[c] for c in sorted(cands,key=(lambda c:approval[c]),reverse=True)])))
 
+    print("\nWinner =", cnames[winner])
     print("\n-----\n")
     return
 
@@ -242,7 +224,7 @@ def main():
     for ballot, w in zip(ballots,weight):
         print(ff.format(w),ballot)
 
-    test_smithrr(ballots, weight, cnames)
+    test_ir3f2(ballots, weight, cnames)
 
 if __name__ == "__main__":
     main()
